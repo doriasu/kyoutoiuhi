2cb89ea40a0e1bff31dd810901d51adcfdc0e2f1
doriasu <takuandtetsu@live.jp>
2021-02-15 21:41:49 +0900

    lintfix

--- pages/calendar.vue
+++ pages/calendar.vue
@@ -22,113 +22,111 @@
           </div>
         </div>
       </div>
-      <popup v-show="showContent" v-on:from-child="closePopup">{{
-        nowComment
-      }}</popup>
+      <popup v-show="showContent" v-on:from-child="closePopup">{{ nowComment }}</popup>
       <twitter />
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import moment from 'moment' 
-import data from 'assets/sample.json' 
-import Vue from 'vue' 
-import popup from '~/components/popup.vue' 
+import moment from 'moment'
+import data from 'assets/sample.json'
+import Vue from 'vue'
+import popup from '~/components/popup.vue'
 import master from '~/components/master.vue'
-import twitter from '~/components/twitter.vue' 
+import twitter from '~/components/twitter.vue'
 export default Vue.extend({
-  components: { popup, master,twitter },
+  components: { popup, master, twitter },
   data() {
     return {
       nowComment: '',
       showContent: false,
       currentDate: moment(),
       commentData: data['data'],
-    } 
+    }
   },
   computed: {
     calendars(): { [name: string]: Number }[][] {
-      return this.getCalendar() 
+      return this.getCalendar()
     },
   },
   methods: {
     getStartDate(): moment.Moment {
-      let date = moment(this.currentDate) 
+      let date = moment(this.currentDate)
       //monthを入れると月の最初の日になる
-      date.startOf('month') 
-      const youbiNum = date.day() 
-      return date.subtract(youbiNum, 'days') 
+      date.startOf('month')
+      const youbiNum = date.day()
+      return date.subtract(youbiNum, 'days')
     },
     getEndDate(): moment.Moment {
-      let date = moment(this.currentDate) 
-      date.endOf('month') 
-      const youbiNum = date.day() 
-      return date.add(6 - youbiNum, 'days') 
+      let date = moment(this.currentDate)
+      date.endOf('month')
+      const youbiNum = date.day()
+      return date.add(6 - youbiNum, 'days')
     },
     getCalendar(): { [name: string]: Number }[][] {
-      let startDate = this.getStartDate() 
-      const endDate = this.getEndDate() 
-      const weekNumber = Math.ceil(endDate.diff(startDate, 'days') / 7) 
+      let startDate = this.getStartDate()
+      const endDate = this.getEndDate()
+      const weekNumber = Math.ceil(endDate.diff(startDate, 'days') / 7)
 
-      let calendars: { [name: string]: Number }[][] = [] 
+      let calendars: { [name: string]: Number }[][] = []
       for (let week = 0; week < weekNumber; week++) {
-        let weekRow: { [name: string]: Number }[] = [] 
+        let weekRow: { [name: string]: Number }[] = []
         for (let day = 0; day < 7; day++) {
           weekRow.push({
             date: startDate.get('date'),
-          }) 
-          startDate.add(1, 'days') 
+          })
+          startDate.add(1, 'days')
         }
-        calendars.push(weekRow) 
+        calendars.push(weekRow)
       }
-      return calendars 
+      return calendars
     },
     nextMonth() {
-      this.currentDate = moment(this.currentDate).add(1, 'month') 
-      return 
+      this.currentDate = moment(this.currentDate).add(1, 'month')
+      return
     },
     prevMonth() {
-      this.currentDate = moment(this.currentDate).subtract(1, 'month') 
-      return 
+      this.currentDate = moment(this.currentDate).subtract(1, 'month')
+      return
     },
     getEval(day: Number) {
-      let evaluation: string = '' 
+      let evaluation: string = ''
       for (let i = 0; i < this.commentData.length; i++) {
-        let data = this.commentData[i] 
+        let data = this.commentData[i]
         if (
           data['year'] == this.currentDate.year() &&
           data['month'] == this.currentDate.month() + 1 &&
           data['day'] == day
         ) {
-          evaluation = data['eval'] 
-          break 
+          evaluation = data['eval']
+          break
         }
       }
-      return evaluation 
+      return evaluation
     },
     popup(day: Number) {
-      let comment: string = '' 
+      let comment: string = ''
       for (let i = 0; i < this.commentData.length; i++) {
-        let data = this.commentData[i] 
+        let data = this.commentData[i]
         if (
           data['year'] == this.currentDate.year() &&
           data['month'] == this.currentDate.month() + 1 &&
           data['day'] == day
         ) {
-          comment = data['comment'] 
-          break 
+          comment = data['comment']
+          break
         }
       }
-      this.nowComment = comment 
-      this.showContent = true 
+      this.nowComment = comment
+      this.showContent = true
     },
     closePopup() {
-      this.showContent = false 
+      this.showContent = false
     },
   },
   mounted() {},
-}) 
+})
 </script>
 <style>
 .calendar {
